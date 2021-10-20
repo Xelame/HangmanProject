34e07f20a1889a5917a6264f09e355d5d3edc210
AROLLAND1 <alexandre.rolland85@ynov.com>
2021-10-20 11:23:28 +0200

    add comments

--- finish.go
+++ finish.go
@@ -1,19 +1,18 @@
 package main
 
-// TODO Add Comments ^^
-
+// Return boolean to know if the game is finished
 func isFinished(numberOfAttempts int, word string) bool {
 	isRunning := true
 	numberOfLetterMissing := 0
-	if numberOfAttempts == 0 {
+	if numberOfAttempts == 0 { // if we haven't no longer attempts
 		isRunning = false
 	}
-	for _, letter := range word {
+	for _, letter := range word { // Count number of underscore
 		if letter == '_' {
 			numberOfLetterMissing++
 		}
 	}
-	if numberOfLetterMissing == 0 {
+	if numberOfLetterMissing == 0 { // if it's remain no more --> all it's appeared
 		isRunning = false
 	}
 	return isRunning
