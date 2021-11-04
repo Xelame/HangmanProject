bfa54457ca936fb0e0664a076ca0ce4f29c14bf3
nathan <nathan.bourry@ynov.com>
2021-11-04 10:41:14 +0100

    restructure

--- hiddenword.go
+++ hiddenword.go
@@ -1,9 +1,32 @@
+/* -----------------------------------------------------------------------------------
+ * Auteur : BOURRY Nathan et Alexandre ROLLAND                     Créer le : 18/10/21
+ * guessing.go                                                          Version : v1.0
+ * Programme qui sert à gerer la partie des choix du joueur (lettre)
+ * ---------------------------------------------------------------------------------*/
+
 package main
 
+// -----------------------------------------------------------------------------------
+// Partie importation librairie
+// -----------------------------------------------------------------------------------
+
+//-------------------------------------------------------------------------------------
+//Partie déclaration des constantes
+//-------------------------------------------------------------------------------------
+
+// -----------------------------------------------------------------------------------
+// Partie déclaration des variables
+// -----------------------------------------------------------------------------------
+
+var hiddenWord = []rune{} // Initialize hiddenword
+var isAlreadySay bool = false
+
+// -----------------------------------------------------------------------------------
+// Partie du programme
+// -----------------------------------------------------------------------------------
+
 func HideWord(word string, listOfLetterAlreadySay *[]rune) string {
-	hiddenWord := []rune{}        // Initialize hiddenword
-	for _, letter := range word { // travel word letter by letter
-		isAlreadySay := false                                    // Bool to know the presence of a letter
+	for _, letter := range word { // travel word letter by letter                                   // Bool to know the presence of a letter
 		for _, letterAppeared := range *listOfLetterAlreadySay { // travel letters memories
 			if ToUpper(letter) == letterAppeared { // Test if letter does be show
 				isAlreadySay = true
