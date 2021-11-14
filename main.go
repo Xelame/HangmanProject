5dd5c328e8ac09e6ba7eaf5a5f2bf863d4373a45
AROLLAND1 <alexandre.rolland85@ynov.com>
2021-11-14 15:39:57 +0100

    Main commented

--- main.go
+++ main.go
@@ -21,36 +21,42 @@ const DOCUMENT_ERROR = "errorGestionary.txt"
 // Program Part
 // -----------------------------------------------------------------------------------
 
+// Run the program
 func main() {
 
 	Menu()
 
 }
 
+// Function to open a file and create a scanner for this file
 func OpenScanner(fileName string) *bufio.Scanner {
-	file, errOpen := os.Open(fileName)
-	if errOpen != nil {
-		errorDectection(errOpen.Error())
-		log.Fatal(TEXT_ERROR_OPEN)
+	file, errOpen := os.Open(fileName) // Open the file
+	if errOpen != nil {                // Error detection
+		ErrorDectection(errOpen.Error())
 	}
-	scanner := bufio.NewScanner(file)
+	scanner := bufio.NewScanner(file) // Create a scanner
 	return scanner
 }
 
-func errorDectection(errDict string) {
+// Function to print the type of the error in a document
+func ErrorDectection(errFile string) {
+	// Create every time the same file to reset at each error dectected
 	file, err := os.Create(DOCUMENT_ERROR)
+	// Test if the creation have an error
 	if err != nil {
 		log.Fatal(err)
 	} else {
-		_, err = file.WriteString(errDict)
+		// Write the type of error detected in this file
+		_, err = file.WriteString(errFile)
 		if err != nil {
 			log.Fatal(err)
 		}
 	}
 }
 
+// Clear the console display
 func Clear() {
-	cmd := exec.Command("clear") //Linux example, its tested
-	cmd.Stdout = os.Stdout
-	cmd.Run()
+	cmd := exec.Command("clear") // Affecte a la variable le chemin permet de lancer les commandes + la string préciser
+	cmd.Stdout = os.Stdout       // Affecte la valeur de sortie a celle de notre console
+	cmd.Run()                    // Execute la commande qui était préciser comme une string
 }
